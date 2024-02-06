5efe9f59d10bd8ec0627dcbd66aa3c0107a257dc
suurgatio <serajsaboungi@ufl.edu>
2024-02-05 23:01:32 -0500

    successfully extracted info from .gpx files using 'gpxpy' library

--- backend/main.py
+++ backend/main.py
@@ -1,2 +1,32 @@
-print("Hello World!");
+import gpxpy
 
+
+def parse_gpx_data(gpx_file_path):
+    """
+    Parses a GPX file to extract relevant data points: SessionDate, Timestamp, Latitude, and Longitude.
+
+    :param gpx_file_path: Path to the GPX file to be parsed.
+    :return: A list of dictionaries, each containing the extracted information for each point.
+    """
+    with open(gpx_file_path, 'r') as gpx_file:
+        gpx = gpxpy.parse(gpx_file)
+
+    data_points = []
+
+    for track in gpx.tracks:
+        for segment in track.segments:
+            for point in segment.points:
+                data_points.append({
+                    'SessionDate': point.time.strftime('%Y-%m-%d'),
+                    'Timestamp': point.time.strftime('%H:%M:%S'),
+                    'Latitude': point.latitude,
+                    'Longitude': point.longitude
+                })
+
+    return data_points
+
+
+# Example usage
+gpx_file_path = '../sample_data/data1.gpx'
+data_points = parse_gpx_data(gpx_file_path)
+print(data_points)
